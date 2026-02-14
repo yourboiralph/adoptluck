c5735076a96c9029af8523e0415e8a49d5626f11
Ralph Hernandez <hernandezralphb@gmail.com>
2026-02-14 23:07:18 +0800

    Deploying to VPS

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -4,6 +4,9 @@ import LongChat from "@/components/chatbox/longchat";
 
 export default function Message() {
     return (
-        <LongChat />
+        // <LongChat />
+        <div>
+            Test
+        </div>
     )
 }
