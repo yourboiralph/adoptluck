c60854938cb65104edd824a216d77b3406902247
Ralph Hernandez <hernandezralphb@gmail.com>
2026-02-15 00:36:09 +0800

    export nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: "export",
   images: {
     remotePatterns: [
         { protocol: "https", hostname: "cdn.playadopt.me" },
