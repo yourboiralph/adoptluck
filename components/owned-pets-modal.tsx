01f50c7a3b66d62de58c33b9b321fa6c27e73bdf
Ralph Hernandez <hernandezralphb@gmail.com>
2026-02-15 02:41:02 +0800

    fixed api endpoint

--- components/owned-pets-modal.tsx
+++ components/owned-pets-modal.tsx
@@ -53,7 +53,7 @@ useEffect(() => {
 
   try {
     const fetchPets = async () => {
-      const res = await fetch('http://localhost:3000/api/user/pets')
+      const res = await fetch('http://localhost:3000/api/pets/user')
       const data = await res.json()
 
       setPets(data.pets)
